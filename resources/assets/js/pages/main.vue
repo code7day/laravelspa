c77b36e698b478284abf2aadf6d18cc0ff8bd0de
Jeffry Jesus De La Rosa <jeffryjesus@gmail.com>
2018-05-14 21:00:47 -0400

    updated package and few changing on main.vue

--- resources/assets/js/pages/main.vue
+++ resources/assets/js/pages/main.vue
@@ -7,25 +7,25 @@
       <b-col sm="6" md="2">
         <div class="social-box instagram">
           <i class="fa fa-wrench"></i>
-            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>{{ $t('profile') }}</strong></b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>Mantenimiento</strong></b-dropdown-item>
         </div>
       </b-col>
        <b-col sm="6" md="2">
         <div class="social-box facebook">
           <i class="fa fa-address-book"></i>
-            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>{{ $t('profile') }}</strong></b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>Estudiantes</strong></b-dropdown-item>
         </div>
       </b-col>
        <b-col sm="6" md="2">
         <div class="social-box twitter">
           <i class="fa fa-list-alt"></i>
-            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>{{ $t('profile') }}</strong></b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>Asistencias</strong></b-dropdown-item>
         </div>
       </b-col>
        <b-col sm="6" md="2">
         <div class="social-box red">
-          <i class="fa fa-wrench"></i>
-            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>{{ $t('profile') }}</strong></b-dropdown-item>
+          <i class="fa fa-graduation-cap "></i>
+            <b-dropdown-item :to="{ name: 'settings.profile' }"><strong>Escuela</strong></b-dropdown-item>
         </div>
       </b-col>
      
